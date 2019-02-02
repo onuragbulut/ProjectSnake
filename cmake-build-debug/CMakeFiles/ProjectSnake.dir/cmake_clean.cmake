c15240f6e8ae960e7114acf5e1636abdb8a90e74
Onur <onura@Onurs-MacBook-Pro.local>
2019-02-02 20:22:34 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/ProjectSnake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectSnake.dir/main.cpp.o"
+  "ProjectSnake.pdb"
+  "ProjectSnake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectSnake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProjectSnake.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProjectSnake.
+# This may be replaced when dependencies are built.
